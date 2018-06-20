ab5a46cafba2297989b6a23860392c2ef0f98b71
randomname8 <rpiscore@gmail.com>
2018-06-20 20:37:14 -0300

    -1 is a better value than 0, as it moves the snowflake to the other end of the spectrum

--- src/main/scala/headache/Snowflake.scala
+++ src/main/scala/headache/Snowflake.scala
@@ -3,5 +3,5 @@ package headache
 object Snowflake {
   def apply(s: String) = java.lang.Long.parseUnsignedLong(s).asInstanceOf[Snowflake]
   def apply(s: Long) = s.asInstanceOf[Snowflake]
-  final val NoSnowflake = Snowflake(0)
+  final val NoSnowflake = Snowflake(-1)
 }
